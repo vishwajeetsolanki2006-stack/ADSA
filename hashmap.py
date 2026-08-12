6eac34a6a3b31b5aed0b35dc7616b3e2280a8dfc
VISHWAJEETSINGH <vishwajeetsolanki2006@gmail.com>
2026-08-12 09:27:48 +0530

    Create hashmap.py

---
+++ hashmap.py
@@ -0,0 +1,26 @@
+class HashMapDemo:
+
+    def __init__(self):
+        self.name = [None] * 10
+        self.roll = [0] * 10
+
+    def put(self, student_name, roll_no):
+        index = roll_no % 10
+
+        self.name[index] = student_name
+        self.roll[index] = roll_no
+
+    def display(self):
+        for i in range(10):
+            if self.name[i] is not None:
+                print("Index:", i,
+                      "Name:", self.name[i],
+                      "Roll No:", self.roll[i])
+
+
+# Main program
+h = HashMapDemo()
+
+h.put("Kaushal", 48)
+
+h.display()
